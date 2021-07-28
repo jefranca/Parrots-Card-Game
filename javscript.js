dc80dfeb3bed06f556afcfc44e1388c1dc3a82a3
Jean Matheus Franca <jean13franca@gmail.com>
2021-07-28 16:34:48 -0300

    Titulo, Cor de Fundo e pergunta quantas cartas

---
+++ javscript.js
@@ -0,0 +1,6 @@
+let qtdCartas = Number(prompt("Com quantas cartas deseja jogar?"));
+
+
+while (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0) {
+    qtdCartas = prompt("Com quantas cartas deseja jogar?");
+}
