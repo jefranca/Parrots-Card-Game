c69e5386b1518d1d9009563dbb22adc90a62db3b
Jean Matheus Franca <jean13franca@gmail.com>
2021-08-01 10:23:55 -0300

    cards layout

--- javscript.js
+++ javscript.js
@@ -1,5 +1,4 @@
-let qtdCartas = Number(prompt("Com quantas cartas deseja jogar?"));
-
+//let qtdCartas = Number(prompt("Com quantas cartas deseja jogar?"));
 
 while (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0) {
     qtdCartas = prompt("Com quantas cartas deseja jogar?");
